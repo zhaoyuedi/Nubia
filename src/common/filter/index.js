7c2629e6dfd81a042509bb9a2fc9e1a2b03aadcc
zhenghy8706 <52476021+zhenghy8706@users.noreply.github.com>
2019-09-14 13:36:45 +0800

    9.14xiawu

--- src/common/filter/index.js
+++
@@ -1,5 +0,0 @@
-import Vue from "vue";
-
-Vue.filter("addUrl", function (value){
-    return "//oss.static.nubia.cn/"+value;
-})
--- src/common/index.js
+++
@@ -1,5 +0,0 @@
-import Vue from "vue";
-
-Vue.filter("addUrl", function (value) {
-    return "https://oss.static.nubia.cn/blockimage" + value;
-})
